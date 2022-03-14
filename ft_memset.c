3a3984d02590def4585a82eab16b285c066fc97f
luciamar1 <101580393+luciamar1@users.noreply.github.com>
2022-03-14 19:20:34 +0100

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,1 +1,22 @@
 
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void    *ft_memset(void *s, int c, size_t n)
+{
+        size_t i;
+
+        i = 0;
+        while (i < n)
+                ((char *)s)[i++] = (unsigned char) c;
+        return (s);
+}
+
+int main (void)
+{
+        char    s[20] = "hola soy super guay\n";
+        ft_memset(s, '-', 5);
+        printf("%s\n", s);
+        return (0);
+}
