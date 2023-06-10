7b010d281a92ea523ddb0e97c2aae1cc897c3b23
luciamar1 <lucia-ma>
2023-06-11 00:55:07 +0200

    strn rev

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucia-ma <lucia-ma@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 02:23:58 by lucia-ma          #+#    #+#             */
+/*   Updated: 2023/06/11 00:54:13 by lucia-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (n == 0)
+		return (0);
+	while (*s1 && *s2 && *s1 == *s2 && --n)
+	{
+		s1 ++;
+		s2 ++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+int	ft_str_rev_n_cmp(const char *s1, const char *s2, size_t n)
+{
+	int len;
+
+	len = ft_strlen(s1) - n;
+	if (n == 0)
+		return (0);
+	while(len --)
+		s1++;
+	while (*s1 && *s2 && *s1 == *s2 && --n)
+	{
+		s1 ++;
+		s2 ++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
