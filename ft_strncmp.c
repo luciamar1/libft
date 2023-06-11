46ceb9dcdeb9b7cb133b031e4e798f7ab202a850
Lucía Martin <101580393+luciamar1@users.noreply.github.com>
2023-06-11 02:04:30 +0200

    strncmp reverse

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -23,3 +23,20 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
+
+int	ft_str_rev_n_cmp(const char *s1, const char *s2, size_t n)
+{
+	int len;
+
+	len = ft_strlen(s1) - n;
+	if (n == 0)
+		return (0);
+	while(len --)
+		s1++;
+	while (*s1 && *s2 && *s1 == *s2 && --n)
+	{
+		s1 ++;
+		s2 ++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
