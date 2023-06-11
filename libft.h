d5cf558871cfa0317142c184b4627dc59f71e4b1
Lucía Martin <101580393+luciamar1@users.noreply.github.com>
2023-06-11 02:05:25 +0200

    strncmp rev

--- libft.h
+++ libft.h
@@ -28,6 +28,7 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+int			ft_str_rev_n_cmp(const char *s1, const char *s2, size_t n);
 void		ft_freecharmatrix(char **matrix);
 void		ft_freeintmatrix(int **matrix, int sec_dimension);
 int			ft_atoi(const char *str);
